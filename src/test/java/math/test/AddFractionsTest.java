fc1967230a4a5cf710abd69ca6126ac17863054a
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-10-09 18:21:11 +0100

    Add non-zero to non-zero.

--- src/test/java/math/test/AddFractionsTest.java
+++ src/test/java/math/test/AddFractionsTest.java
@@ -27,4 +27,10 @@ public class AddFractionsTest {
 
 		assertEquals(5, sum.intValue());
 	}
+
+	@Test
+	public void NonZerPlusNonZero() {
+		Fraction sum = new Fraction(3). plus(new Fraction(4));
+		assertEquals(7, sum.intValue());
+	}
 }
